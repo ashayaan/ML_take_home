b5984111114979bb760aeec7e565834749693ab0
induilanchezian <induilanchezian@gmail.com>
2017-12-11 14:18:54 +0530

    Code

--- 1.py
+++ 1.py
@@ -6,15 +6,39 @@ from sklearn import svm
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 
+
+#####SVM Hyperparameter space#####
+kernel = ['rbf','linear','poly','sigmoid','precomputed'}
+C = [0.5,1.0, 1.5, 2.0, 2.5, 3.0]
+degree = np.arange(2,10)
+shrinking = [True, False]
+tol = [1e-5,1e-4,1e-3,1e-2,1e-1]
+class_weight = [None, 'balanced']
+max_iter = np.arange(0,1e3+1,1e2)
+
+#####Logistic Regression Hyperparameter Space######
+
+
 def genParams(baseList, blender, P, N):
 	numBaseModels = len(baseList)
 	mu = np.random.dirichlet(P)
 	numModelsList = N * mu
-	#hyperparameters for the model
-	#hyperparameters for belending model
-	return numModelsList
+	for i in np.arange(len(numModelsList)):
+		if baseList[i] == 'SVM':
+			for j in np.arange(numModelsList[i]):
+				#randomly sample each hyperparameter - C, kernel type, degree, gamma, coef0, shrinking, tol, cache_size, class_weight, max_iter, decision_function_shape, random_state
+				#probability parameter should always be true
+				
+		elif baseList[i] == 'LR':
+			for j in np.arange(numModelsList[i]):
+				#randomly sample each hyperparameter - 
+		elif baseList[i] == 'RF':
+			for j in np.arange(numModelsList[i]):
+				parameters = {}
+		else:
+	return numModelsList,parameterListBaseModels, parameterListBlenderModel 
 
 def Blend():
 	
 
-
+	
